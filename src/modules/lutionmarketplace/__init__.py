073f13f5952c2225c05f8db6853d8d471c5f0b56
cihp4 <itsjustchipoverhere@proton.me>
2025-08-08 11:49:49 +0000

    lution marketplace search

---
+++ src/modules/lutionmarketplace/__init__.py
@@ -0,0 +1,45 @@
+import json
+import os
+from dotenv import load_dotenv
+from github import Github
+from github import Auth
+
+load_dotenv()
+auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
+
+g = Github(auth=auth)
+repo = g.get_repo("Wookhq/Lution-Marketplace")
+
+class LutionMarketplace:
+    def __init__(self):
+        self.themedata = repo.get_contents("Assets/Themes/content.json")
+        self.moddata = repo.get_contents("Assets/Mods/content.json")
+        
+
+    def get_themes(self):
+        content = self.themedata
+        data = json.loads(content.decoded_content.decode())
+        themes = [theme["title"] for theme in data if "title" in theme]
+        return themes
+
+    def get_mods(self):
+        content = self.moddata
+        data = json.loads(content.decoded_content.decode())
+        mods = [mod["title"] for mod in data if "title" in mod]
+        return mods
+
+    def get_theme_description(self, title):
+        content = self.themedata
+        data = json.loads(content.decoded_content.decode())
+        for item in data:
+            if item.get("title") == title:
+                return item.get("body", None)
+            
+
+    def get_mod_description(self, mod):
+        content = self.moddata
+        data = json.loads(content.decoded_content.decode())
+        for item in data:
+            if item.get("title") == mod:
+                return item.get("body", None)
+    

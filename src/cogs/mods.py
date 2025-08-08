d463929de6195b71210bc1e7fdb3e655b0865daa
cihp4 <itsjustchipoverhere@proton.me>
2025-08-08 09:46:48 +0000

    unban + kick

--- src/cogs/mods.py
+++ src/cogs/mods.py
@@ -30,6 +30,62 @@ class Mods(commands.Cog):
 
         else:
             await ctx.send("You don't have permission to ban members.")
+    @commands.command()
+    async def unban(self, ctx, *, member: str = None):
+        if member is None:
+            file = discord.File(get_random_shitpost())
+            await ctx.send(file=file)
+        elif ctx.author.guild_permissions.ban_members:
+            try:
+                # check bot perms
+                if not ctx.guild.me.guild_permissions.ban_members:
+                    raise PermissionError("I can't unban, no perms")
+                
+                banned_users = await ctx.guild.bans()
+                member_name, member_discriminator = member.split("#")
+
+                for ban_entry in banned_users:
+                    user = ban_entry.user
+                    if (user.name, user.discriminator) == (member_name, member_discriminator):
+                        await ctx.guild.unban(user)
+                        embed = discord.Embed(title="Unbanned!",
+                                description=f"Unbanned {user.mention}",
+                                colour=0x00b0f4)
+
+                        await ctx.send(embed=embed)
+                        return
+
+                await ctx.send(f"{member} not found in the ban list.")
+            except Exception as e:
+                file = discord.File(get_random_shitpost())
+                await ctx.send(file=file)
+                print(e)
+        else:
+            await ctx.send("You don't have permission to unban members.")
+
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member = None, *, reason: str = "No reason provided"):
+        if member is None:
+            file = discord.File(get_random_shitpost())
+            await ctx.send(file=file)
+        elif ctx.author.guild_permissions.kick_members:
+            try:
+                # check bot perms  
+                if not ctx.guild.me.guild_permissions.kick_members:
+                    raise PermissionError("I can't kick, no perms")
+                await member.kick(reason=reason)
+                embed = discord.Embed(title="Kicked!",
+                        description=f"Kicked {member.mention} for {reason}",
+                        colour=0x00b0f4)
+
+                await ctx.send(embed=embed)
+            except Exception as e:
+                file = discord.File(get_random_shitpost())
+                await ctx.send(file=file)
+                print(e)
+        else:
+            await ctx.send("You don't have permission to kick members.")
+
 
 async def setup(bot):
     await bot.add_cog(Mods(bot))
